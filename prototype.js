c14e74a6f6366662f77f7c93c60622c3678271af
shubhamraicopythat <quicktalkme@gmail.com>
2025-04-25 08:22:51 +0530

    prototypes

---
+++ prototype.js
@@ -0,0 +1,11 @@
+let myHeros = ["spiderman", "ironman", "thor"];
+let Dcheros = ["batman", "Superman", "Flash"];
+
+let herorpower = {
+  spiderman: "sling",
+  thor: "Hamering ",
+  ironman: "fliing",
+  getspidemanpower: function () {
+    console.log("Spideman power is ${this.spiderman}");
+  },
+};
